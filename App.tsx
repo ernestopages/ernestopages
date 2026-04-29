c7689175ec2c46f1a038d4c5ba0885f39090d629
ernestopages <ernestohenriquefotos@gmail.com>
2026-04-29 18:25:41 -0300

    feat: Update checkout link
    Changes the checkout link to a new one for the product.

--- App.tsx
+++ App.tsx
@@ -114,7 +114,7 @@ const App: React.FC = () => {
     document.getElementById('offer-section')?.scrollIntoView({ behavior: 'smooth' });
   };
 
-  const FINAL_LINK = "https://indec-digital.mycartpanda.com/checkout/203161373:1";
+  const FINAL_LINK = "https://indec-digital.mycartpanda.com/checkout/209838814:1";
 
   const testimonialImages = [
     "https://i.ibb.co/tTytHwBv/Depo-1.webp",
